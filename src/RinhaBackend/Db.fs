1e30c73626daf5852648c45b29a0926c4b7f2bec
Raphael Camine <raphael.camine@gmail.com>
2024-02-13 20:42:00 -0300

    wip

--- src/RinhaBackend/Db.fs
+++ src/RinhaBackend/Db.fs
@@ -13,18 +13,18 @@ type Db(connString: string) =
         conn
         |> Db.newCommand
             "CREATE TABLE cliente (
-                                id integer PRIMARY KEY NOT NULL,
-                                saldo integer NOT NULL,
-                               limite integer NOT NULL);
+                id integer PRIMARY KEY NOT NULL,
+                saldo integer NOT NULL,
+                limite integer NOT NULL);
 
-                             CREATE TABLE transacao (
-                                id SERIAL PRIMARY KEY,
-                                valor integer NOT NULL,
-                                descricao varchar(10) NOT NULL,
-                                realizadaem timestamp NOT NULL,
-                                idcliente integer NOT NULL);
+             CREATE TABLE transacao (
+                id SERIAL PRIMARY KEY,
+                valor integer NOT NULL,
+                descricao varchar(10) NOT NULL,
+                realizadaem timestamp NOT NULL,
+                idcliente integer NOT NULL);
 
-                             CREATE INDEX ix_transacao_idcliente ON transacao (idcliente ASC);"
+             CREATE INDEX ix_transacao_idcliente ON transacao (idcliente ASC);"
         |> Db.exec
 
     member _.ResetDb() =
@@ -33,12 +33,12 @@ type Db(connString: string) =
         conn
         |> Db.newCommand
             "delete from transacao;
-                            delete from cliente;
-                            INSERT INTO cliente (id, saldo, limite) VALUES (1, 0, -100000);
-                            INSERT INTO cliente (id, saldo, limite) VALUES (2, 0, -80000);
-                            INSERT INTO cliente (id, saldo, limite) VALUES (3, 0, -1000000);
-                            INSERT INTO cliente (id, saldo, limite) VALUES (4, 0, -10000000);
-                            INSERT INTO cliente (id, saldo, limite) VALUES (5, 0, -500000);"
+             delete from cliente;
+             INSERT INTO cliente (id, saldo, limite) VALUES (1, 0, -100000);
+             INSERT INTO cliente (id, saldo, limite) VALUES (2, 0, -80000);
+             INSERT INTO cliente (id, saldo, limite) VALUES (3, 0, -1000000);
+             INSERT INTO cliente (id, saldo, limite) VALUES (4, 0, -10000000);
+             INSERT INTO cliente (id, saldo, limite) VALUES (5, 0, -500000);"
         |> Db.exec
 
     //TODO: should consider credit/debit aswell
@@ -47,14 +47,14 @@ type Db(connString: string) =
 
         try
             conn
-            |> Db.newCommand "UPDATE clientes SET saldo = saldo + @valor WHERE id = @customerId"
+            |> Db.newCommand "UPDATE cliente SET saldo = saldo + @valor WHERE id = @customerId"
             |> Db.setParams
                 [ "@valor", SqlType.Int transaction.Amount
                   "@customerId", SqlType.Int transaction.Customer.CustomerId ]
             |> Db.exec
 
             conn
-            |> Db.newCommand "SELECT saldo, limite FROM clientes WHERE id = @customerId"
+            |> Db.newCommand "SELECT saldo, limite FROM cliente WHERE id = @customerId"
             |> Db.setParams [ "@customerId", SqlType.Int transaction.Customer.CustomerId ]
             |> Db.querySingle (fun rd ->
                 { transaction with
@@ -72,7 +72,7 @@ type Db(connString: string) =
 
         try
             conn
-            |> Db.newCommand "SELECT id, saldo, limite FROM clientes WHERE id = @customerId"
+            |> Db.newCommand "SELECT id, saldo, limite FROM cliente WHERE id = @customerId"
             |> Db.setParams [ "@customerId", SqlType.Int customerId ]
             |> Db.querySingle (fun rd -> Customer.create customerId (rd.ReadInt32 "saldo") (rd.ReadInt32 "limite"))
             |> Ok
